3e78d0a281d5b3b6c6f913188ab0cdc3c8d55e98
채은 <ea21y@naver.com>
2025-01-31 18:18:44 +0900

    feat/login

---
+++ src/Components/GoogleButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Login.css"; // 같은 스타일 파일 사용
+
+const GoogleButton = () => {
+  return (
+    <button className="google-button">
+      <img src="/google.png" alt="Sign in with Google" className="google-image" />
+    </button>
+  );
+};
+
+export default GoogleButton;

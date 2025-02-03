998165b8a64a321023663d9c094b310333c89777
박소영 <pegasus@ewha.ac.kr>
2025-02-04 00:58:55 +0900

    feat: 메인페이지의 드롭다운 기능 구현

---
+++ src/components/Main/DropDown.jsx
@@ -0,0 +1,51 @@
+export default function DropDown() {
+  return (
+    <>
+      <select name="selectedYear">
+        <option value="none" hidden>
+          학년
+        </option>
+        <option value="freshman">1학년</option>
+        <option value="sophomore">2학년</option>
+        <option value="junior">3학년</option>
+        <option value="senior">4학년</option>
+      </select>
+      <select name="selectedCollege">
+        <option value="none" hidden>
+          단과대학
+        </option>
+        <option value="humanities">인문과학대학</option>
+        <option value="social">사회과학대학</option>
+        <option value="natural">자연과학대학</option>
+        <option value="engineering">공과대학</option>
+        <option value="music">음악대학</option>
+        <option value="arts">조형예술대학</option>
+        <option value="education">사범대학</option>
+        <option value="business">경영대학</option>
+        <option value="convergence">신산업융합대학</option>
+        <option value="medical">의과대학</option>
+        <option value="nursing">간호대학</option>
+        <option value="pharmacy">약학대학</option>
+        <option value="scranton">스크랜튼대학</option>
+        <option value="ai">인공지능대학</option>
+        <option value="hokma">호크마교양대학</option>
+      </select>
+      <select name="selectedIncome">
+        <option value="none" hidden>
+          소득분위
+        </option>
+        <option value="0">0분위</option>
+        <option value="1">1분위</option>
+        <option value="2">2분위</option>
+        <option value="3">3분위</option>
+        <option value="4">4분위</option>
+        <option value="5">5분위</option>
+        <option value="6">6분위</option>
+        <option value="7">7분위</option>
+        <option value="8">8분위</option>
+        <option value="9">9분위</option>
+        <option value="10">10분위</option>
+      </select>
+    </>
+  );
+}

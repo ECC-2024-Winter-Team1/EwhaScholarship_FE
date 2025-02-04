f49ab139ba46b943658788209f591ade1d06bca9
박소영 <pegasus@ewha.ac.kr>
2025-02-04 16:45:11 +0900

    feat: 아이콘 삽입

--- src/components/Main/SearchBox.jsx
+++ src/components/Main/SearchBox.jsx
@@ -1,9 +1,22 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
-const InputWrapper = styled.div`
+const Wrapper = styled.div`
   display: flex;
   justify-content: center;
-  gap: 10px;
+  gap: 5px;
+`;
+
+const InputWrapper = styled.div`
+  position: relative;
+`;
+
+const IconStyle = styled.i`
+  position: absolute;
+  padding: 10px 20px;
+  font-size: 20px;
+  color: #00462a;
 `;
 
 const StyledInput = styled.input`
@@ -11,23 +24,28 @@ const StyledInput = styled.input`
   border-radius: 20px;
   border: 2px solid #00462a;
   font-size: 25px;
-  padding: 10px 10px 10px 35px;
+  padding: 10px 10px 10px 45px;
 `;
 
 const Button = styled.button`
-  width: 80px;
+  width: 100px;
   border-radius: 20px;
   font-size: 25px;
   background-color: #00462a;
   color: white;
-  padding: 10px 10px;
+  padding: 10px 20px;
 `;
 
 export default function SearchBox() {
   return (
-    <InputWrapper>
-      <StyledInput placeholder="궁금한 장학금 이름을 검색하세요" />
+    <Wrapper>
+      <InputWrapper>
+        <IconStyle>
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+        </IconStyle>
+        <StyledInput placeholder="궁금한 장학금 이름을 검색하세요" />
+      </InputWrapper>
       <Button>검색</Button>
-    </InputWrapper>
+    </Wrapper>
   );
 }

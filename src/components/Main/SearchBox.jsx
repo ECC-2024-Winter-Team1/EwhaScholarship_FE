47cc77ae5314928b9bf18f5169ce9e67f30b7448
박소영 <pegasus@ewha.ac.kr>
2025-02-04 16:02:13 +0900

    feat: 검색창 화면 가운데 정렬 및 기본적인 꾸밈

--- src/components/Main/SearchBox.jsx
+++ src/components/Main/SearchBox.jsx
@@ -1,10 +1,27 @@
 import styled from "styled-components";
 
-const InputWrapper = styled.div``;
+const InputWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+`;
 
-const StyledInput = styled.input``;
+const StyledInput = styled.input`
+  width: 900px;
+  border-radius: 20px;
+  border: 2px solid #00462a;
+  font-size: 25px;
+  padding: 10px 10px 10px 35px;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  width: 80px;
+  border-radius: 20px;
+  font-size: 25px;
+  background-color: #00462a;
+  color: white;
+  padding: 10px 10px;
+`;
 
 export default function SearchBox() {
   return (

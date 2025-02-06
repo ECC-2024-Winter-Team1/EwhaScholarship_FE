cee5a126b76392f8cdb1007f03eb6f1566f233a8
박소영 <pegasus@ewha.ac.kr>
2025-02-06 19:50:12 +0900

    feat: 메인페이지 등록금 정보 디자인까지 구현

--- src/components/Pagination/Content.jsx
+++ src/components/Pagination/Content.jsx
@@ -1,23 +1,43 @@
-import { BoxWrapper, Box } from "./Content.style";
+import { BoxWrapper, Box, LinkBox, TextWrapper } from "./Content.style";
 
 export default function Content() {
   return (
     <BoxWrapper>
       <Box>
-        <h2>이화미래설계</h2>
-        <p>최대 400만원 | 3월/9월 | 학업보조비</p>
+        <TextWrapper>
+          <h2>이화미래설계</h2>
+          <p>최대 400만원 | 3월/9월 | 학업보조비</p>
+        </TextWrapper>
+        <LinkBox>
+          <a href="#">자세히 보기</a>
+        </LinkBox>
       </Box>
       <Box>
-        <h2>전공리더십</h2>
-        <p>학과별 상이 | 4월/10월 | 학비감면</p>
+        <TextWrapper>
+          <h2>전공리더십</h2>
+          <p>학과별 상이 | 4월/10월 | 학비감면</p>
+        </TextWrapper>
+        <LinkBox>
+          <a href="#">자세히 보기</a>
+        </LinkBox>
       </Box>
       <Box>
-        <h2>이화복지</h2>
-        <p>차등지급 | 11월/5월 | 학비감면</p>
+        <TextWrapper>
+          <h2>이화복지</h2>
+          <p>차등지급 | 11월/5월 | 학비감면</p>
+        </TextWrapper>
+        <LinkBox>
+          <a href="#">자세히 보기</a>
+        </LinkBox>
       </Box>
       <Box>
-        <h2>등록금 옴부즈만</h2>
-        <p>최대 400만원 | 2월/8월 | 학비감면</p>
+        <TextWrapper>
+          <h2>등록금 옴부즈만</h2>
+          <p>최대 400만원 | 2월/8월 | 학비감면</p>
+        </TextWrapper>
+        <LinkBox>
+          <a href="#">자세히 보기</a>
+        </LinkBox>
       </Box>
     </BoxWrapper>
   );
--- src/components/Pagination/Content.style.jsx
+++ src/components/Pagination/Content.style.jsx
@@ -3,14 +3,45 @@ import styled from "styled-components";
 export const BoxWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 5px;
+  gap: 30px;
 `;
 
 export const Box = styled.div`
   display: flex;
-  flex-direction: column;
-  width: 1300px;
+  flex-direction: row;
+  align-items: center;
+  gap: 50%;
+  width: 1200px;
   margin: 0 auto;
-  border: 2px solid red;
-  padding: 10px 10px;
+  border-radius: 25px;
+  box-shadow: 1px 5px 5px 1px rgb(164, 177, 162);
+  background-color: #f0f7f4;
+  padding: 10px 20px;
+`;
+
+export const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1000px;
+
+  h2 {
+    margin-bottom: 5px;
+  }
+`;
+
+export const LinkBox = styled.div`
+  display: flex;
+  align-items: center;
+  width: 200px;
+  height: 50px;
+  background-color: #00462a;
+  border-radius: 25px;
+  padding: 10px 15px;
+
+  a {
+    text-decoration-line: none;
+    color: white;
+    font-size: 18px;
+    font-weight: 530;
+  }
 `;

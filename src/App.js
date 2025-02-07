c35544cdbcab6256fdac48f286b26f2513663829
ansu05 <suahn05@ewha.ac.kr>
2025-02-07 18:22:11 +0900

    feat: 리뷰 목록 보여주기 기능 뼈대 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import logo from "./logo.svg";
-import "./App.css";
-import MainPage from "./components/Main/MainPage";
+<<<<<<< Updated upstream
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return <MainPage />;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+=======
+import ReviewForm from "./Components/DetailPage/ReviewForm";
+import ReviewList from "./Components/DetailPage/ReviewList";
+
+function App() {
+  return (
+    <div>
+      <header>
+>>>>>>> Stashed changes
+      </header>
+        <ReviewForm />
+        <ReviewList />
+    </div>
+  );
 }
 
 export default App;

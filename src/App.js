28c26777b7ca1ffe8b9e8b2fe00e673a87b7187d
ansu05 <suahn05@ewha.ac.kr>
2025-02-07 18:31:18 +0900

    fix: App.js 초기 내용 삭제

--- src/App.js
+++ src/App.js
@@ -1,24 +1,3 @@
-<<<<<<< Updated upstream
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-=======
 import ReviewForm from "./Components/DetailPage/ReviewForm";
 import ReviewList from "./Components/DetailPage/ReviewList";
 
@@ -26,7 +5,6 @@ function App() {
   return (
     <div>
       <header>
->>>>>>> Stashed changes
       </header>
         <ReviewForm />
         <ReviewList />

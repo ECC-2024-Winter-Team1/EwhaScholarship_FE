c3b3c1fdc3738c47a64ca336254208ed6fadb7ed
박소영 <pegasus@ewha.ac.kr>
2025-02-09 17:57:18 +0900

    feat: 페이지네이션 구상한대로 다시 구현

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 export function Pagination({
   postsNum,
   postsPerPage,
@@ -6,17 +7,28 @@ export function Pagination({
 }) {
   const pageList = [];
   const totalPages = Math.ceil(postsNum / postsPerPage);
+  const pagesPerGroup = 5;
+  const totalPageGroup = Math.ceil(totalPages / pagesPerGroup);
+  const [currentPageGroup, setCurrentPageGroup] = useState(0);
+  const startPageIndex = pagesPerGroup * currentPageGroup + 1;
+  const endPageIndex = Math.min(startPageIndex + pagesPerGroup - 1, totalPages);
 
-  for (let i = 1; i <= totalPages; i++) {
+  for (let i = startPageIndex; i <= endPageIndex; i++) {
     pageList.push(i);
   }
 
-  const goToNextPage = () => {
-    setCurrentPage(currentPage + 1);
+  const goToNextPageGroup = () => {
+    if (currentPageGroup < totalPageGroup - 1) {
+      setCurrentPageGroup(currentPageGroup + 1);
+      setCurrentPage(startPageIndex + pagesPerGroup);
+    }
   };
 
-  const goToPrevPage = () => {
-    setCurrentPage(currentPage - 1);
+  const goToPrevPageGroup = () => {
+    if (currentPageGroup > 0) {
+      setCurrentPageGroup(currentPageGroup - 1);
+      setCurrentPage(startPageIndex - pagesPerGroup);
+    }
   };
 
   if (totalPages === 1) {
@@ -25,22 +37,21 @@ export function Pagination({
 
   return (
     <div>
-      <button onClick={goToPrevPage} disabled={currentPage === 1}>
-        prev
+      <button onClick={goToPrevPageGroup} disabled={currentPageGroup === 0}>
+        {"<"}
       </button>
 
       {pageList.map((page) => (
-        <button
-          key={page}
-          onClick={() => setCurrentPage(page)}
-          className={currentPage === page ? "active" : ""}
-        >
+        <button key={page} onClick={() => setCurrentPage(page)}>
           {page}
         </button>
       ))}
 
-      <button onClick={goToNextPage} disabled={currentPage === pageList.length}>
-        next
+      <button
+        onClick={goToNextPageGroup}
+        disabled={currentPageGroup === totalPageGroup - 1}
+      >
+        {">"}
       </button>
     </div>
   );

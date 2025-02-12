39936afc76f812d9e198189636620371e775a55d
채은 <ea21y@naver.com>
2025-02-12 15:25:53 +0900

    feat : 아이디, 비밀번호를 사용한 일반 로그인 전체 기능 구현

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,25 +1,85 @@
-import React from "react";
-import GoogleButton from "./GoogleButton";
-import { Container, TitleContainer, Subtitle, Title, Description } from "./Login.style";
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { 
+    LoginContainer, LoginForm, LoginTitle, LoginSubtitle, Description,
+    InputLabel, Group, Item, SubmitButton, Icon2, InputStyle1, StyledLink
+} from "./Login.style";
+import axios from "axios";
 
 const Login = () => {
-  return (
-    <Container> 
-        <TitleContainer>
-            <Subtitle>
-                EWHA Scholar
-            </Subtitle>
-            <Title>
-                이화장학
-            </Title>
-        </TitleContainer>
-        <Description>
-            로그인하시고 <br /> 받을 수 있는 장학금을 찾아보세요!
-        </Description>
-        <GoogleButton />
-    </Container>
-  )
-}
+    const [id, setId] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginCheck, setLoginCheck] = useState(false); 
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+
+        e.preventDefault();
+
+        try {
+            const response = await axios.post(
+                "로그인서버주소넣을공간",
+                {
+                    id: id,
+                    password: password,
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+
+            const result = response.data;
+
+            if (response.status === 200) {
+                setLoginCheck(false); 
+                sessionStorage.setItem("token", result.token);
+                sessionStorage.setItem("id", result.id);
+                navigate("/"); 
+            } 
+            else {
+                setLoginCheck(true); 
+                alert("아이디와 비밀번호가 일치하지 않습니다."); 
+            }
+        } catch (error) {
+            setLoginCheck(true); 
+            alert("로그인 오류가 발생했습니다. 다시 시도해주세요."); 
+        }
+    };
+
+    return (
+        <LoginContainer>
+            <LoginForm>
+                <LoginTitle>EWHA Scholar</LoginTitle>
+                <LoginSubtitle>이화장학</LoginSubtitle>
+
+                <Group>
+                    <Item>
+                        <Icon2 src="/profile.png" alt="Icon"/>
+                        <InputLabel htmlFor="id">아이디</InputLabel>
+                        <InputStyle1 
+                            value={id} onChange={(e) => setId(e.target.value)}
+                            type="text" id="form_id" name="form_id"/>
+                    </Item>
+                    <Item>
+                        <Icon2 src="/key.png" alt="Icon"/>
+                        <InputLabel htmlFor="password">비밀번호</InputLabel>
+                        <InputStyle1 
+                            value={password} onChange={(e) => setPassword(e.target.value)}
+                            type="password" id="form_password" name="form_password" />
+                    </Item>
+                </Group>
+
+                <SubmitButton type="submit" onClick={handleLogin}>
+                    로그인
+                </SubmitButton>
+            </LoginForm>
+            <Description>
+               <StyledLink to="/join">회원가입하기</StyledLink>
+            </Description>
+        </LoginContainer>
+    );
+};
 
 export default Login;
--- src/Components/Login/Login.style.jsx
+++ src/Components/Login/Login.style.jsx
@@ -1,84 +1,116 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  width: 100vw;
-  height: 100vh;
-  background-color: #fff;
+export const Icon2 = styled.img`
+    width: 20x;
+    height: 20px;
+    margin-left: 7px;
 `;
 
-const TitleContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+export const LoginContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    background-color: #ffffff;
+    font-family: 'Pretendard', sans-serif;
+`;
+
+export const LoginForm = styled.div`
+    text-align: center;
+    width: 18rem;
+`;
+
+export const LoginTitle = styled.h1`
+    font-size: 1rem;
+    font-weight: 340;
+    color: #00462A;
+    margin: 0;
+    padding: 0;
+    line-height: 1;
+    width: 105%;
+`;
+
+export const LoginSubtitle = styled.h2`
+    width: 105%;
+    font-family: 'SeoulNamsanM', sans-serif;
+    font-weight: 730;
+    font-size: 2.15rem;
+    color: #00462A;
+    margin: 5px;
+    padding: 0;
+    line-height: 1;
+    margin-bottom: 35px;
+`;
+
+
+export const InputLabel = styled.label`
+    font-size: 1rem;
+    font-weight: 500;
+    color: #333333;
+    width: 100%;
+    text-align: left;
+`;
+
+export const InputStyle1 = styled.input`
+    width: 190%;
+    padding: 12px 16px;
+    border: 1px solid #00462A;
+    border-radius: 12px;
+    background-color: white;
+    font-size: 0.875rem;
+    outline: none;
+`;
+
+export const Group = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: 105%;
+`;
+
+export const Item = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 14px;
+    width: 100%;
+    height: 40px;
+`;
+
+export const SubmitButton = styled.button`
+    width: 105%;
+    background-color: #00462A;
+    color: white;
+    padding: 12px;
+    margin-top: 24px;
+    font-size: 1rem;
+    font-weight: 500;
+    border: none;
+    border-radius: 0;
+    cursor: pointer;
+`;
+
+export const Description = styled.p`
   position: absolute;
-  top: 270px;
-  width: 427px;
-  height: 96px;
-`;
-
-const Subtitle = styled.h1`
-  font-size: 1rem;
-  font-weight: 300; 
-  color: #00462A;
-  margin: 0;
-  padding: 0;
-  line-height: 1; 
-`;
-
-const Title = styled.h2`
-  font-family: 'SeoulNamsanM', sans-serif;
-  font-weight: 700;
-  font-size: 2.15rem;
-  color: #00462A;
-  margin: 5px;
-  padding: 0;
-  line-height: 1;
-  margin-bottom: 24px;
-`;
-
-const Description = styled.p`
-  position: absolute;
-  top: 337px;
+  top: 520px;
+  padding-left: 13px;
   width: 348px;
   text-align: center;
   font-family: "Pretendard", sans-serif;
   font-weight: 500;
-  font-size: 16.8px;
+  font-size: 14px;
   line-height: 21px;
   color: #333333;
-`;
-
-const Button = styled.button`
-  position: absolute;
-  top: 411px;
-  width: 249px;
-  height: 60px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: transparent;
-  border: none;
   cursor: pointer;
+  text-decoration: underline;
 `;
 
-const Image = styled.img`
-  width: 63%;
-  height: 63%;
+
+export const StyledLink = styled(Link)`
+  color: #333333;
+  text-decoration: underline;
   
+  &:hover {
+    color: gray;
+  }
 `;
-
-export {
-    Container,
-    TitleContainer,
-    Subtitle, 
-    Title,
-    Description,
-    Button,
-    Image
-};

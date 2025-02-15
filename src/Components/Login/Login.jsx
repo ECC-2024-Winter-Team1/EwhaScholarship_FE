8df185af955c54ddeca8a8b68d9aee99b4ee02c9
ansu05 <suahn05@ewha.ac.kr>
2025-02-15 22:13:27 +0900

    feat: Login.jsx 파일 원래대로 수정

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,19 +1,8 @@
 import React from "react";
 import GoogleButton from "./GoogleButton";
 import { Container, TitleContainer, Subtitle, Title, Description } from "./Login.style";
-import { useNavigate } from "react-router-dom";
 
-//채은님 로그인 화면 완성본과 많이 다름 내가 바꾼 이 파일은 어떻게 둬야 나중에 머지할 때 편할까 이 파일은 필요없는데//
 const Login = () => {
-    const navigate = useNavigate();
-
-    const handleLoginSuccess = () => {
-        navigate("/main");
-    };
-
-    const moveToJoin = () => {
-        navigate("/join");
-    }
 
     return (
         <Container> 
@@ -28,8 +17,7 @@ const Login = () => {
             <Description>
                 로그인하시고 <br /> 받을 수 있는 장학금을 찾아보세요!
             </Description>
-            <GoogleButton onLoginSuccess={handleLoginSuccess} />
-            <button onClick={moveToJoin}>회원가입</button>
+            <GoogleButton />
         </Container>
     );
 };

587e41eb7245b6a3e2cafdf05d89caef1a256b45
hslee <hanseul.dev@gmail.com>
2025-02-18 01:05:52 +0900

    feat: 회원가입 유효성 검사 추가 및 리팩토링

---
+++ src/Components/Join/join.const.js
@@ -0,0 +1,37 @@
+export const DEPARTMENTS = [
+  { value: "인문과학대학", label: "인문과학대학" },
+  { value: "사회과학대학", label: "사회과학대학" },
+  { value: "자연과학대학", label: "자연과학대학" },
+  { value: "(엘텍)공과대학", label: "(엘텍)공과대학" },
+  { value: "음악대학", label: "음악대학" },
+  { value: "조형예술대학", label: "조형예술대학" },
+  { value: "사범대학", label: "사범대학" },
+  { value: "경영대학", label: "경영대학" },
+  { value: "신산업융합대학", label: "신산업융합대학" },
+  { value: "의과대학", label: "의과대학" },
+  { value: "간호대학", label: "간호대학" },
+  { value: "약학대학", label: "약학대학" },
+  { value: "스크랜튼대학", label: "스크랜튼대학" },
+  { value: "인공지능대학", label: "인공지능대학" },
+  { value: "호크마교양대학", label: "호크마교양대학" },
+];
+
+export const YEARS = [
+  { value: "1", label: "1학년" },
+  { value: "2", label: "2학년" },
+  { value: "3", label: "3학년" },
+  { value: "4", label: "4학년" },
+];
+
+export const INCOMELEVELS = [
+  { value: "1", label: "1분위" },
+  { value: "2", label: "2분위" },
+  { value: "3", label: "3분위" },
+  { value: "4", label: "4분위" },
+  { value: "5", label: "5분위" },
+  { value: "6", label: "6분위" },
+  { value: "7", label: "7분위" },
+  { value: "8", label: "8분위" },
+  { value: "9", label: "9분위" },
+  { value: "10", label: "10분위" },
+];

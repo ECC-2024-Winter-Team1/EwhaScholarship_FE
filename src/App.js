d738175c5152d9f08479f0fb7139fea4d64c882c
박소영 <pegasus@ewha.ac.kr>
2025-02-22 20:16:51 +0900

    feat: 오류 고침

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import "./index.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import DetailPage from "./Components/DetailPage/DetailPage";
+import DetailPage from "./components/DetailPage/DetailPage";
 import Login from "./components/Login/Login";
 import Join from "./components/Join/Join";
 import BookMark from "./components/BookMark/BookMark";
-import MainPage from "./components/Pagination/ShowingPagination";
+import MainPage from "./components/Pagination/MainPage";
 import Info from "./components/Info/Info";
 
 function App() {
